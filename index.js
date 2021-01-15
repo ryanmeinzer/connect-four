fb80dfa55016a77f2258fb0789ddf966a9820413
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-14 19:04:57 -0800

    validated checkWin function upon every click, beginning checkWin logic

--- index.js
+++ index.js
@@ -12,22 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (let i = 0; i < squares.length; i++) {
         squares[i].onclick = function (e) {
-            // alert('hi')
             if (currentPlayer === 1) {
                 player1Array.push(e.target.id)
                 displayPlayer1Moves.innerHTML = player1Array
+                checkWin()
                 currentPlayer = 2
                 displayCurrentPlayer.innerHTML = currentPlayer
             } else {
                 player2Array.push(e.target.id)
                 displayPlayer2Moves.innerHTML = player2Array
+                checkWin()
                 currentPlayer = 1
                 displayCurrentPlayer.innerHTML = currentPlayer
             }
-
-
-
-
         }
     }
 
@@ -38,4 +35,14 @@ function checkWin() {
     let wins = [
         [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [4, 8, 12, 16], [1, 6, 11, 16], [4, 7, 10, 13]
     ]
+
+    alert('hi')
+
+    // // more logic needed
+    // for (let i = 0; i < wins.length; i++) {
+    //     let square1 = squares[wins[i][0]]
+    //     let square2 = squares[wins[i][1]]
+    //     let square3 = squares[wins[i][2]]
+    //     let square4 = squares[wins[i][3]]
+    // }
 }

363a7f5a80efa288d35799fb8cabe7c8853a71f1
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-14 18:32:28 -0800

    adding logic for clicks within grid

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 let turn = "player1"
 
 document.addEventListener('DOMContentLoaded', () => {
-    let squares = 
+    let squares = document.querySelectorAll('.grid div')
+    let result = document.querySelector('#result')
+    let displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].onclick = function () {
+            alert('hi')
+        }
+    }
+
 
 })
 
-Player 1 - clicks square
-onClick =
-    getID = player1Moves.push(id)
-turn.push()
-if player1 clicks, disable cursor / click
-evaluate if winCombos.includes(player1Moves)
-if so, won!, else continue game
+// Psuedocode
+// Player 1 - clicks square
+// onClick =
+//     getID = player1Moves.push(id)
+// turn.push()
+// if player1 clicks, disable cursor / click
+// evaluate if winCombos.includes(player1Moves)
+// if so, won!, else continue game
 
-[repeat for player2]
-turn = [2]
+// [repeat for player2]
+// turn = [2]
 
-let player1Moves = [2, 6, 9, 5]
-let player2Moves = [4, 8, 15, 3]
+// let player1Moves = [2, 6, 9, 5]
+// let player2Moves = [4, 8, 15, 3]
 
-[2, 6, 9, 5] != [2, 6, 10, 14]
+// [2, 6, 9, 5] != [2, 6, 10, 14]
 
-loop through, evaluation each of the four.includes()
+// loop through, evaluation each of the four.includes()
 
-win[9]
+// win[9]
 
-player1Moves.sort().toString().concat()
+// player1Moves.sort().toString().concat()
 
 
 
-let winCombos = { [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], }
-
-/*
+// let winCombos = { [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], }

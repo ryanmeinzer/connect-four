d2faf80389df11445c5929f00d992c6fc835366c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-14 18:44:19 -0800

    added player array logic upon click

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
     let result = document.querySelector('#result')
     let displayCurrentPlayer = document.querySelector('#current-player')
     let currentPlayer = 1
+    let displayPlayer1Moves = document.querySelector('#player-1-moves')
+    let displayPlayer2Moves = document.querySelector('#player-2-moves')
+    let player1Array = []
+    let player2Array = []
 
     for (let i = 0; i < squares.length; i++) {
-        squares[i].onclick = function () {
-            alert('hi')
+        squares[i].onclick = function (e) {
+            // alert('hi')
             if (currentPlayer === 1) {
+                player1Array.push(e.target.id)
+                displayPlayer1Moves.innerHTML = player1Array
                 currentPlayer = 2
                 displayCurrentPlayer.innerHTML = currentPlayer
             } else {
+                player2Array.push(e.target.id)
+                displayPlayer2Moves.innerHTML = player2Array
                 currentPlayer = 1
                 displayCurrentPlayer.innerHTML = currentPlayer
             }
